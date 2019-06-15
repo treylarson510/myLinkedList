985c3300c124df53de840c43972c179dc637db38
treylarson510 <45209852+treylarson510@users.noreply.github.com>
2019-06-15 10:13:26 -0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -13,7 +13,7 @@ template <class P>
 class LinkedList
 {
 //nt numLists = 0;
-protected:
+public:
 	//constructor--------------------------------------------------------------------
 	LinkedList() {
 		top = nullptr;
@@ -76,6 +76,62 @@ protected:
 		}
 
 	}
+
+	//will add a new element at index given
+	//0 to numNodes-1
+	//adding at 0 index will add at the top
+	//adding at numNodes index will add at the end
+	void addAtIndex(int index, P newPayload) {
+		assert(index >= 0);
+		assert(index <= numNodes);
+		if (index < 0|| index > numNodes){
+			throw runtime_error("Illegal  addAtIndex()");
+		}
+		if (index == 0) {//if user wants to add at index 0
+			Node<P> *newNode = new Node <P>(newPayload, nullptr);//temporary node used
+
+			if (end == nullptr) {
+				top = newNode; ++numNodes;
+				end = newNode;
+			}
+			else {
+				top = new Node <P>(newPayload, top); ++numNodes;
+			}
+		}
+		else if (index == numNodes) {//if user wants to add after the last element
+			//create new node
+			Node<P> *newNode = new Node <P>(newPayload, nullptr);
+
+			if (numNodes == 0) {//if there arent any nodes already in the queue
+				top = newNode; end = newNode; ++numNodes;//sets top and end to the same node
+			}
+			else {//otherwise it will add the node to the end, not effecting top and moving end to the next end node
+				end->setNext(newNode); end = newNode; ++numNodes;
+
+			}
+		}
+		else {
+			try {
+				//pointer to travers list to desired index, starts at the top
+				Node <P> *curr = top;
+				Node <P>*next = new Node<P>(newPayload, nullptr);
+				for (int i = 0; i < index-1; i++) {
+					curr = curr->getNext();
+				}
+				//create new node
+				next->setNext(curr->getNext());
+				curr->setNext(next);	numNodes++;
+				//curr->setPayload(newPayload);
+
+			}
+			catch (exception &e)
+			{
+				cerr << "Caught: " << e.what() << endl;
+				cerr << "Type: " << typeid(e).name() << endl;
+			}
+		}
+	}
+
 	//removing methods---------------------------------------------------------------------
 	P removeTop() {
 		assert(top != nullptr);
@@ -98,26 +154,61 @@ protected:
 		if (end == nullptr) {
 			throw runtime_error("Illegal pop()");
 		}
+		Node <P> *temp = top;
+
+		while (temp->getNext() !=end) {
+			temp = temp->getNext();
+		}
 		P returnVal = end->getPayload();
-		Node<P> *temp = end->getNext();
-		delete end;	numNodes--;
+		delete (end);	numNodes--;
 		end = temp;
+		end->setNext(nullptr);
 		return returnVal;
 	}
 
+	//will remove element at index given
+	//0 to numNodes-1
+	//remove at 0 index will removeTop
+	//removing at numNodes -1 will remove the end
+	P removeAtIndex(int index) {
+		assert(index >= 0);
+		assert(index < numNodes);
+		P returnVal;
+		if (index < 0 || index >= numNodes) {
+			throw runtime_error("Illegal  removeAtIndex()");
+		}
+		if (index == 0) {//if user wants to remove index 0
+			returnVal = removeTop();
+		}
+		else if (index == numNodes-1) {//if user wants to remove the last index
+			returnVal=removeEnd();
+		}
+		else {
+				//pointer to travers list to desired index, starts at the top, destroy will be the deleted index
+				Node <P> *destroy, *curr = top;
+				for (int i = 0; i < index-1; i++) {
+					curr = curr->getNext();
+				}
+				destroy = curr->getNext();
+				returnVal = destroy->getPayload();
+				curr->setNext(destroy->getNext());
+				delete(destroy); numNodes--;
+		}
+		return returnVal;
+	}
 	//accessors-----------------------------------------------------------------------------
 	P readTop() const {
-		assert(top != nullptr);
+		/*assert(top != nullptr);
 		if (top == nullptr) {
 			throw runtime_error("Illegal readTop()");
-		}
+		}*/
 		return top->getPayload();
 	}
 
 	P readEnd() const {
-		asser(end != nullptr);
+		/*asser(end != nullptr);
 		if (end == nullptr)
-			throw runtime_error("Illegal readEnd()");
+			throw runtime_error("Illegal readEnd()");*/
 		return end->getPayload;
 	}
 	//Number of nodes in the list
@@ -178,4 +269,3 @@ private:
 
 
 };
-
